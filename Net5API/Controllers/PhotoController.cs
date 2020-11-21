8fc56ab1072a14e4799077b6cac1b5f56f129e30
Marthin Silalahi <marthin.silalahi@anacle.com>
2020-11-21 22:09:24 +0800

    INITIAL PROJECT & GIT IGNORE

---
+++ Net5API/Controllers/PhotoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net5API.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Net5API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private HttpClient httpClient;
+        private string endpoint;
+
+        public PhotoController()
+        {
+            httpClient = new HttpClient();
+            endpoint = "https://jsonplaceholder.typicode.com/photos";
+        }
+
+        [HttpGet]
+        [Route("method1")]
+        public async Task<IEnumerable<Photo>> Method1Async()
+        {
+            using var response = await httpClient.GetAsync(endpoint, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+
+            if (response.Content is object)
+            {
+                var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                return await JsonSerializer.DeserializeAsync<List<Photo>>(stream).ConfigureAwait(false);
+            }
+
+            return null;
+        }
+
+        [HttpGet]
+        [Route("method2")]
+        public async Task<IEnumerable<Photo>> Method2Async()
+        {
+            return await httpClient.GetFromJsonAsync<List<Photo>>(endpoint).ConfigureAwait(false);
+        }
+    }
+}
---
+++ Net5API/Models/Photo.cs
@@ -0,0 +1,11 @@
+﻿namespace Net5API.Models
+{
+    public class Photo
+    {
+        public int albumId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string thumbnailUrl { get; set; }
+    }
+}
